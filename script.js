92e10193cd53c8cb2384b2531896d57a50c95ee5
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-01-08 01:56:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ document.getElementById('openModal').addEventListener('click', openModal);
+
+  function openModal() {
+    document.getElementById('myModal').style.display = 'flex';
+  }
+
+  function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+  }
+
+  window.onclick = function(event) {
+    if (event.target === document.getElementById('myModal')) {
+      closeModal();
+    }
+  };
